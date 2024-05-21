85b84336d6fca3e382d68f7b3030716cb1bd2ade
Александр Гасымов <gazykh@yandex.ru>
2024-05-21 17:47:40 +0300

    First solution

---
+++ task1/solution_1.py
@@ -0,0 +1,18 @@
+def circular_array(n, m):
+    n_list = list(range(1, n + 1))
+    arrays = []
+    index = 0
+
+    while True:
+        short_list = [n_list[(index + i) % n] for i in range(m)]
+        arrays.append(short_list[0])
+        index = (index + m - 1) % n
+        if index == 0 and len(arrays) > 1:
+            break
+
+    return arrays
+
+
+n, m = map(int, input().split())
+resulting_array = circular_array(n, m)
+print("".join(map(str, resulting_array)))
